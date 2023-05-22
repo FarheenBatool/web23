46553a1195bcf71b003419c2c8143e1dc0c267a8
FarheenBatool <farheenbatool43@gmail.com>
2023-05-22 13:45:11 +0500

    done

--- Assignments/Assignment2(FA20-BCS-001)/express.js
+++ Assignments/Assignment2(FA20-BCS-001)/express.js
@@ -1,8 +1,33 @@
 const express = require("express");
 const server = express();
+const path = require("path");
+const mongoose = require("mongoose");
 
-server.use(express.static("public"));
+server.use(express.json());
 
-server.listen(3000, () => {
-  console.log("Server Started, Visit localhost:3000");
+
+server.use("/", require("./public/routes/api/stores"));
+
+const publicPath = path.join(__dirname, "public");
+const viewsPath = path.join(publicPath, "views");
+
+server.set("view engine", "ejs");
+server.set("views", viewsPath);
+server.use(express.static(publicPath));
+
+// Route handler
+server.get("/", (req, res) => {
+  res.render("index");
 });
+
+mongoose
+  .connect("mongodb://localhost/project", { useNewUrlParser: true })
+  .then(() => {
+    console.log("Connected to MongoDB");
+    server.listen(3000, () => {
+      console.log("Server started, visit localhost:3000");
+    });
+  })
+  .catch((error) => {
+    console.log("Error connecting to MongoDB:", error.message);
+  });

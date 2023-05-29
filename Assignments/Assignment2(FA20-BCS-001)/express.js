efa8a778e81365f89171e6cb5987fd36bc1da455
FarheenBatool <farheenbatool43@gmail.com>
2023-05-29 17:26:38 +0500

    trying

--- Assignments/Assignment2(FA20-BCS-001)/express.js
+++ Assignments/Assignment2(FA20-BCS-001)/express.js
@@ -1,19 +1,18 @@
 const express = require("express");
 const server = express();
-const path = require("path");
+//const path = require("path");
 const mongoose = require("mongoose");
 
 server.use(express.json());
 
 
-server.use("/", require("./public/routes/api/stores"));
+server.use("/", require("./routes/api/stores"));
 
-const publicPath = path.join(__dirname, "public");
-const viewsPath = path.join(publicPath, "views");
 
 server.set("view engine", "ejs");
-server.set("views", viewsPath);
-server.use(express.static(publicPath));
+server.use(express.static("public"))
+//server.set("views", viewsPath);
+//server.use(express.static(publicPath));
 
 // Route handler
 server.get("/", (req, res) => {

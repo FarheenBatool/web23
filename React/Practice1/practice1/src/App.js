29eb3da6d22f12a7bde34740b2c9f44ae2ba2771
FarheenBatool <farheenbatool43@gmail.com>
2023-07-03 13:16:54 +0500

    props

--- React/Practice1/practice1/src/App.js
+++ React/Practice1/practice1/src/App.js
@@ -1,10 +1,21 @@
+import React from 'react';
+
 import './App.css';
+import Bar from './components/Bar';
+
+
 
 function App() {
   return (
+    <>
     <div className="App">
       <h1>Hi From Frin </h1>
     </div>
+    <Bar title="Text Utilss"/>
+   
+   
+   </>
+
   );
 }
 

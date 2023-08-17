5c7861c2475e9363a3ebe805532fc4b472c641e2
FarheenBatool <farheenbatool43@gmail.com>
2023-08-17 20:40:41 +0500

    Marks as Done [IMPLEMENTED]

--- React/Todo/todo/src/App.js
+++ React/Todo/todo/src/App.js
@@ -1,15 +1,20 @@
 import { useState,useEffect, useRef } from 'react';
 import List from './components/List';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faPlus} from '@fortawesome/free-solid-svg-icons';
+import { v4 as uuidv4 } from 'uuid';
+
+import "./App.css"
 
 
 function App() {
-   const [count,setCount]= useState(0)
+  //  const [count,setCount]= useState(0)
    const [todo,setTodo]= useState('')
    const [list,setList]= useState([])
    const countRef = useRef(0)
 
    function addList(){
-    setList((prevlist)=> [...prevlist,todo])
+    setList((prevlist)=> [...prevlist,{id:uuidv4(),name:todo,completed:false}])
     setTodo('')
     console.log(list)
     }
@@ -20,45 +25,38 @@ function App() {
    
    
 
-   useEffect(() => {
-    //setCount(prevCount => prevCount+1) 
-    // render hota ha , state update hoti h , state update ha phr useEffect render hota , infinite loop
-    countRef.current = countRef.current + 1
-   })
+  //  useEffect(() => {
+  //   //setCount(prevCount => prevCount+1) 
+  //   // render hota ha , state update hoti h , state update ha phr useEffect render hota , infinite loop
+  //   countRef.current = countRef.current + 1
+  //  })
    
-   function Decrement() {
-    // this works
-    setCount(prevCount => prevCount -1) 
-    setCount(prevCount => prevCount -1)
+  //  function Decrement() {
+  //   // this works
+  //   setCount(prevCount => prevCount -1) 
+  //   setCount(prevCount => prevCount -1)
+  //  }
 
-    
-   }
-   function Increment(){
-    // this does not
-    // here it is just over riding , count is the value when we render the function 2 plus 1 3 , again 2 plus 1 is three
-    setCount(count + 1)
-    setCount(count + 1)
+  //  function Increment(){
+  //   // this does not
+  //   // here it is just over riding , count is the value when we render the function 2 plus 1 3 , again 2 plus 1 is three
+  //   setCount(count + 1)
+  //   setCount(count + 1)
 
-   }
+  //  }
   return (
   <>
-  {/* <button onClick={Decrement}>-</button>
-  <span>{count}</span>
-  <button onClick={Increment}>+</button> */}
-  {/* <div>
-  <List/>
-  <button>Edit</button>
-  <button>Delete </button>
+  <div className='head'>
+  <h2 style={{color:'black'}}>Enter Todo's</h2>
+  <div>
+  <input type='text' placeholder='Enter Todo' value={todo} onChange={e => setTodo(e.target.value)}/>
+  <FontAwesomeIcon className='icon' icon={faPlus} onClick={addList} />
+  </div>
+  <List list = {list} updateList={updateList}/>
 
   </div>
-  <input placeholder='Enter Text'/>
-  <button>Add</button> */}
 
-  <input placeholder='Enter Todo' value={todo} onChange={e => setTodo(e.target.value)}/>
-  {/* <div>My name is {name}</div>
-  <div>Rerender {countRef.current}</div> */}
-  <button onClick={addList}>Add</button>
-  <List list = {list} updateList={updateList}/>
+ 
 
   </>
   );
--- React/Todo/todo/src/components/List.js
+++ React/Todo/todo/src/components/List.js
@@ -1,41 +1,60 @@
 import React, { useState,useEffect } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faTrash} from '@fortawesome/free-solid-svg-icons';
+import { faCircleCheck} from '@fortawesome/free-regular-svg-icons';
+
+
+import '../App.css'
 
 export default function List(props) {
-  // const { list } = props.List;
-  // console.log(props.list)
-  const array = props.list
-  console.log(array)
-  const [updatelist , setUpdatelist]= useState(props.list) 
-  console.log("hi",updatelist)
+  const [updatelist , setUpdatelist]= useState([]) 
+
+  // console.log("hi",updatelist)
+
   useEffect(() => {
     setUpdatelist(props.list);
   }, [props.list]);
-   function done(){
 
+   function done(index){
+    // const doneTodo =[...updatelist]
+    // const todo = doneTodo.find(todo => todo.id === index)
+    // todo.completed = !todo.completed
+    const updatedList = updatelist.map(todo => {
+      if (todo.id === index) {
+        return {
+          ...todo,
+          completed: !todo.completed
+        };
+      }
+      return todo;
+    });
+    setUpdatelist(updatedList)
+    props.updateList(updatedList)
   }
   
   function del(index) {
-    const newList = updatelist.filter((_, i) => i !== index);
+    const newList = updatelist.filter(todo => todo.id !== index);
     setUpdatelist(newList);
     props.updateList(newList);
 
   }
-  //  useEffect(() => {
-  //   if (props.list.length === 0) {
-  //     setUpdatelist([]);
-  //   }
-  // }, [props.list]);
-  
   return (
-    <ul>
+    <ol>
       {updatelist.map((item, index) => (
         <>
-        <li key={index}>{item}</li>
-        <button onClick={() =>del(index)}>Delete</button>
-        <button onClick={done}>Complete</button>
+        <div className='listDiv' >
+        <li className={`li ${item.completed ? 'completed' : ''}`} key={item.id}>{item.name}</li>
+        <div className='iconContainer'>
+        <FontAwesomeIcon className='icon' icon={faTrash} onClick={() =>del(item.id)}  />
+        <FontAwesomeIcon className='icon' icon={faCircleCheck} onClick={() =>done(item.id)}/>
+        </div> 
+        {/* <button onClick={() =>del(index)}>Delete</button> */}
+        {/* <button onClick={done}>Complete</button> */}
+       </div>
+       
         </>
      
       ))}
-    </ul>
+    </ol>
   );
 }

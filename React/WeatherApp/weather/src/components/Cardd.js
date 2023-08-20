f10d4036e40b0b485c3b9c05eefa0f46092cc6b3
FarheenBatool <farheenbatool43@gmail.com>
2023-08-20 22:59:05 +0500

    [Styles Applied to Weather}

--- React/WeatherApp/weather/src/components/Cardd.js
+++ React/WeatherApp/weather/src/components/Cardd.js
@@ -1,20 +1,55 @@
 import React from 'react'
-import Card from 'react-bootstrap/Card';
+import {Card,InputGroup,Form} from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faMagnifyingGlass,faCloud} from '@fortawesome/free-solid-svg-icons';
+
+
+
+
+
 
 
 export default function Carddd() {
   return (
-    <Card className="bg-dark text-white">
-    <Card.Img src="holder.js/100px270" alt="Card image" />
-    <Card.ImgOverlay>
-      <Card.Title>Card title</Card.Title>
-      <Card.Text>
-        This is a wider card with supporting text below as a natural lead-in
-        to additional content. This content is a little bit longer.
-      </Card.Text>
-      <Card.Text>Last updated 3 mins ago</Card.Text>
-    </Card.ImgOverlay>
-  </Card>
+    <div className='container mt-5 '>
+      <div className='row justify-content-center'> 
+        <div className='col-md-4'>
+          <Card className="bg-dark text-white text-center border-0 ">
+          <Card.Img src="https://plus.unsplash.com/premium_photo-1669809948017-518b5d800d73?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80" alt="Card image" />
+          <Card.ImgOverlay>
+            <form>
+              <InputGroup className="mb-4 w-75 mx-auto">
+                <Form.Control
+                  type='search'
+                  placeholder="Search City"
+                  aria-label="Search City"
+                  aria-describedby="basic-addon2"
+                />
+          <button type='submit' className='input-group-text' id="basic-addon2">
+          <FontAwesomeIcon icon={faMagnifyingGlass} />
+          </button>
+        </InputGroup>
+            </form>
+            <div className='bg-dark bg-opacity-50 py-3'>
+            <Card.Title className="mb-4"  style={{ fontSize: '2rem' }} >London  </Card.Title>
+            <Card.Text className='lead'>
+              Friday, December 1 , 2002
+            </Card.Text>
+            <hr/>
+            <FontAwesomeIcon icon={faCloud} style={{color: "#ebebeb",fontSize: '3rem'}} />
+            <h1>33.0 &deg;C</h1>
+            <p className='lead fw-bolder mb-0'>Cloud</p>
+            <p className='lead'>30.01 &deg;C | 37.02 &deg;C</p>
+            </div>
+          
+          </Card.ImgOverlay>
+          </Card>  
+
+        </div>
+      </div>
+    </div>
+   
     
   )
 }
